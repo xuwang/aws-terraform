85dc35d7304a53572d30629e7d65c36302502c38
Xu Wang <xuwang@gmail.com>
2015-07-08 19:39:40 -0700

    fix setup-s3-prefix.sh

--- coreos-cluster/s3/setup-s3-prefix.sh
+++ coreos-cluster/s3/setup-s3-prefix.sh
@@ -3,5 +3,5 @@
 AWS_ACCOUNT=$(aws --profile coreos-cluster iam get-user --user-name=coreos-cluster | jq ".User.Arn" | grep -Eo '[[:digit:]]{12}')
 
 cd ..
-perl -pi -e "s/AWS-ACCOUNT/$AWS_ACCOUNT/g" */*.tf */*.json
+perl -pi.bak -e "s/AWS-ACCOUNT/$AWS_ACCOUNT/g" */*.tf */*.json
 
