57be98015085efc7ff639ea9d0ce89848136936a
Xu Wang <xuwang@gmail.com>
2015-07-13 19:04:14 -0700

    update substitute-AWS-ACCOUNT.sh
    use <file>.tmpl as template file, and generates new file with the orginal name without the .tmpl suffix.

--- scripts/substitute-AWS-ACCOUNT.sh
+++ scripts/substitute-AWS-ACCOUNT.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
-# Replace AWS-ACCOUNT with aws account number in *.tf and *.json files. Can be used 
-# to form policy documents, resource names etc, with the aws account number as part of string.
-# A backup of the original file will be created.
+# Replace AWS-ACCOUNT with aws account number in *.tf.tmpl and *.json.tmpl files. Can be used 
+# to form policy documents, resource names etc, with the aws account number as part of a string.
+# If a substitusion is made, a new file will be created with the original name with out the .tmpl sufix.
 
 # Script location
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # AWS account number - getting it from an existing user resource
+echo "Getting AWS account number..."
 AWS_ACCOUNT=$(aws --profile coreos-cluster iam get-user --user-name=coreos-cluster | jq ".User.Arn" | grep -Eo '[[:digit:]]{12}')
-cd ${DIR}/../coreos-cluster
-files=$(grep -s -l AWS-ACCOUNT -r --include *.tf --include *.json .)
-for i in $files
-do
-  echo "perl -pi.bak -e "s/AWS-ACCOUNT/$AWS_ACCOUNT/g" $i" 
-done
+workdir=${DIR}/../coreos-cluster
+cd $workdir
+files=$(grep -s -l AWS-ACCOUNT -r --include *.tmpl --include *.tmpl .)
+if [ "X$files" != "X" ];
+then
+  for i in $files
+  do
+    path=$(dirname $i)
+    newfile=$(basename $i .tmpl)
+    echo "generating $newfile"
+    perl -p -e "s/AWS-ACCOUNT/$AWS_ACCOUNT/g" $i > $workdir/$path/$newfile
+  done
+fi
 
