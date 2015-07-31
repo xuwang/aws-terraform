7dad7ddd881081ffb361cce40d0298497a727082
Xu Wang <xuwang@gmail.com>
2015-07-31 10:15:24 -0700

    update iam to define tf variables in seperate variables.tf file

---
+++ modules/iam/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_account_id" { }
+variable "aws_account_region" { }
+variable "aws_account_user" { }
+variable "deployment_user" { default = "deployment" }
+variable "cloud_config_file_path" { default = "cloud-config/aws-files.yaml" }
--- resources/terraforms/module-iam.tf
+++ resources/terraforms/module-iam.tf
@@ -3,5 +3,4 @@ module "iam" {
     deployment_user = "deployment"
     aws_account_id = "${var.aws_account.id}"
     aws_account_user = "${var.aws_account.user}"
-    cloudinit_file_path = "cloud-config/aws-files.yaml"
 }
