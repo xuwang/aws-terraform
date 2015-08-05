54f74a9ecea013947ef4c9468c50d7dc59ac1377
Xu Wang <xuwang@gmail.com>
2015-08-05 16:02:28 -0700

    ssh-add keypair from the build/keypairs directory.

--- scripts/create-and-upload-keypair.sh
+++ scripts/create-and-upload-keypair.sh
@@ -18,14 +18,15 @@ then
     echo "keypair ${key} already exists."
 else
     mkdir -p ${TMP_DIR}
+    chmod 700 ${TMP_DIR}
     echo "Creating ${key} and uploading to s3"
     aws --profile ${AWS_PROFILE} ec2 create-key-pair --key-name ${key} --query 'KeyMaterial' --output text > ${TMP_DIR}/${key}.pem
     aws --profile ${AWS_PROFILE} s3 cp ${TMP_DIR}/${key}.pem s3://${AWS_ACCOUNT}-${CLUSTER_NAME}-config/keypairs/${key}.pem
     # copy the key to user's home .ssh
-    cp ${TMP_DIR}/${key}.pem ${HOME}/.ssh; chmod 600 ${HOME}/.ssh/${key}.pem
-    echo "ssh-add ${HOME}/.ssh/${key}.pem"
-    ssh-add ${HOME}/.ssh/${key}.pem
+    # cp ${TMP_DIR}/${key}.pem ${HOME}/.ssh; chmod 600 ${HOME}/.ssh/${key}.pem
+    chmod 600 ${TMP_DIR}/${key}.pem
+    echo "ssh-add ${TMP_DIR}/${key}.pem"
+    ssh-add ${TMP_DIR}/${key}.pem
     # Clean up
     # rm -rf ${TMP_DIR}
 fi
-
