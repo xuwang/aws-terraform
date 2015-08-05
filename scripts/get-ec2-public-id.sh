77553c485554548c0da13aa87d279dd2804786a1
Xu Wang <xuwang@gmail.com>
2015-08-05 15:09:53 -0700

    Update AWS_PROFILE and AWS_REGION variables.

--- scripts/get-ec2-public-id.sh
+++ scripts/get-ec2-public-id.sh
@@ -4,8 +4,8 @@ AWS_REGION=${2:-us-west-2}
 AWS_PROFILE=${3:-coreos-cluster}
 ASG=${1:-etcd}
 
-EC2_IDS=$(aws --profile $PROFILE autoscaling describe-auto-scaling-groups \
-        --region $REGION --auto-scaling-group-name $ASG \
+EC2_IDS=$(aws --profile $AWS_PROFILE autoscaling describe-auto-scaling-groups \
+        --region $AWS_REGION --auto-scaling-group-name $ASG \
     | jq .AutoScalingGroups[0].Instances[].InstanceId | xargs)
 
 aws --profile $AWS_PROFILE ec2 describe-instances --region $AWS_REGION --instance-ids $EC2_IDS \
