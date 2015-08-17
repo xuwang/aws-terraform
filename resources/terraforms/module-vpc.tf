a0d1c2b580dcb3e58edd1a732b1032c10ea14af1
Xu Wang <xuwang@gmail.com>
2015-08-16 17:08:50 -0700

    add example of how to override default vpc subnets

--- resources/terraforms/module-vpc.tf
+++ resources/terraforms/module-vpc.tf
@@ -3,25 +3,25 @@ module "vpc" {
     vpc_cidr = "10.0.0.0/16"
     vpc_name = "${var.cluster_name}"
 /*
-# can be configured:
-    variable "etcd_subnet_a" { default = "10.0.1.0/26" }
-    variable "etcd_subnet_b" { default = "10.0.1.64/26" }
-    variable "etcd_subnet_c" { default = "10.0.1.128/26" }
+# the following can be configured. See ../modules/vpc for default values
+    etcd_subnet_a = <cdir value>
+    etcd_subnet_b = <cdir value>
+    etcd_subnet_c = <cdir value>
 
-    variable "admiral_subnet_a" { default = "10.0.10.0/24" }
-    variable "admiral_subnet_b" { default = "10.0.11.0/24" }
-    variable "admiral_subnet_c" { default = "10.0.12.0/24" }
+    admiral_subnet_a = <cdir value>
+    admiral_subnet_b = <cdir value>
+    admiral_subnet_c = <cdir value>
     
-    variable "worker_subnet_a" { default = "10.0.20.0/24" }
-    variable "worker_subnet_b" { default = "10.0.21.0/24" }
-    variable "worker_subnet_c" { default = "10.0.22.0/24" }
+    worker_subnet_a = <cdir value>
+    worker_subnet_b = <cdir value>
+    worker_subnet_c = <cdir value>
 
-    variable "elb_subnet_a" { default = "10.0.30.0/24" }
-    variable "elb_subnet_b" { default = "10.0.31.0/24" }
-    variable "elb_subnet_c" { default = "10.0.32.0/24" }
+    elb_subnet_a = <cdir value>
+    elb_subnet_b = <cdir value>
+    elb_subnet_c = <cdir value>
 
-    variable "rds_subnet_a" { default = "10.0.40.0/24" }
-    variable "rds_subnet_b" { default = "10.0.41.0/24" }
-    variable "rds_subnet_c" { default = "10.0.42.0/24" }
+    rds_subnet_a = <cdir value>
+    rds_subnet_b = <cdir value>
+    rds_subnet_c = <cdir value>
 */
 }
