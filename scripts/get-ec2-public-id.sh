0298a5f83df732750f948555b5be64d059d9394a
Xu Wang <xuwang@gmail.com>
2015-09-03 21:44:45 -0700

    use the default the profile default region

--- scripts/get-ec2-public-id.sh
+++ scripts/get-ec2-public-id.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-AWS_REGION=${2:-us-west-2}
-AWS_PROFILE=${3:-coreos-cluster}
+AWS_PROFILE=${AWS_PROFILE:-coreos-cluster}
 ASG=${1:-etcd}
 
-EC2_IDS=$(aws --profile $AWS_PROFILE autoscaling describe-auto-scaling-groups \
-        --region $AWS_REGION --auto-scaling-group-name $ASG \
+EC2_IDS=$(aws --profile $AWS_PROFILE autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG \
     | jq .AutoScalingGroups[0].Instances[].InstanceId | xargs)
 
-aws --profile $AWS_PROFILE ec2 describe-instances --region $AWS_REGION --instance-ids $EC2_IDS \
+aws --profile $AWS_PROFILE ec2 describe-instances --instance-ids $EC2_IDS \
     | jq -r '.Reservations[].Instances | map(.NetworkInterfaces[].Association.PublicIp)[]'
