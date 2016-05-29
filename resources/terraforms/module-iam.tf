9228637db4e35e7b957b7973a2cbdb851748b94f
Xu Wang <xuwang@gmail.com>
2016-05-29 00:08:02 -0700

    iam: upload keys to s3 config bucket.

--- resources/terraforms/module-iam.tf
+++ resources/terraforms/module-iam.tf
@@ -2,7 +2,7 @@ module "iam" {
     source = "../modules/iam"
     deployment_user = "deployment"
     aws_account_id = "${var.aws_account.id}"
-    aws_account_user = "${var.aws_account.user}"
     aws_account_region = "${var.aws_account.default_region}"
     cluster_name = "${var.cluster_name}"
+    config-bucket = "${module.s3.s3_bucket_config_id}"
 }
