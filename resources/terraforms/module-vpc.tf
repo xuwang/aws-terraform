1a78c80bc1fb4e79a0fdee573c206c4fc65d7a5e
Xu Wang <xuwang@gmail.com>
2016-05-29 00:07:10 -0700

    customize vpc and subnets.

--- resources/terraforms/module-vpc.tf
+++ resources/terraforms/module-vpc.tf
@@ -1,32 +1,29 @@
 module "vpc" {
     source = "../modules/vpc"
-    vpc_cidr = "10.0.0.0/16"
+    vpc_cidr = "10.10.0.0/16"
     vpc_name = "${var.cluster_name}"
     vpc_region = "${var.aws_account.default_region}"
-    
-/*
-# the following can be configured. See ../modules/vpc for default values
-    etcd_subnet_a = <cdir value>
-    etcd_subnet_b = <cdir value>
-    etcd_subnet_c = <cdir value>
 
-    admiral_subnet_a = <cdir value>
-    admiral_subnet_b = <cdir value>
-    admiral_subnet_c = <cdir value>
-    
-    worker_subnet_a = <cdir value>
-    worker_subnet_b = <cdir value>
-    worker_subnet_c = <cdir value>
+    # Override the default configuration. See ../modules/vpc for default values
+    etcd_subnet_a = "10.10.1.0/26"
+    etcd_subnet_b = "10.10.1.64/26"
+    etcd_subnet_c = "10.10.1.128/26"
 
-    elb_subnet_a = <cdir value>
-    elb_subnet_b = <cdir value>
-    elb_subnet_c = <cdir value>
+    admiral_subnet_a = "10.10.2.0/26"
+    admiral_subnet_b = "10.10.2.64/26"
+    admiral_subnet_c = "10.10.2.128/26"
 
-    rds_subnet_a = <cdir value>
-    rds_subnet_b = <cdir value>
-    rds_subnet_c = <cdir value>
-*/
+    elb_subnet_a = "10.10.3.0/26"
+    elb_subnet_b = "10.10.3.64/26"
+    elb_subnet_c = "10.10.3.128/26"
 
+    rds_subnet_a = "10.10.4.0/26"
+    rds_subnet_b = "10.10.4.64/26"
+    rds_subnet_c = "10.10.4.128/26"
+
+    worker_subnet_a = "10.10.5.0/26"
+    worker_subnet_b = "10.10.5.64/26"
+    worker_subnet_c = "10.10.5.128/26"
 
     etcd_subnet_az_a  = "${var.aws_account.default_region}a"
     etcd_subnet_az_b  = "${var.aws_account.default_region}b"
