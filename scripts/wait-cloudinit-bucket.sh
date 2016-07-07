39af2fd285403a4fb67ea945de02af118f96d7de
Xu Wang <xuwang@gmail.com>
2016-07-06 21:08:50 -0700

    added retry.

--- scripts/wait-cloudinit-bucket.sh
+++ scripts/wait-cloudinit-bucket.sh
@@ -5,9 +5,15 @@ CLUSTER_NAME=${CLUSTER_NAME:-coreos-cluster}
 AWS_ACCOUNTID=$(aws --profile ${AWS_PROFILE} iam get-user | jq ".User.Arn" | grep -Eo '[[:digit:]]{12}')
 BUCKET="${AWS_ACCOUNTID}-${CLUSTER_NAME}-cloudinit"
 KEY="etcd/initial-cluster"
-if ! aws --profile $AWS_PROFILE s3api wait object-exists --bucket ${BUCKET} --key ${KEY};
-then
-  echo "${BUCKET}/initial-cluster doesn't exist"
-  exit 1
-fi
-exit 0
+retry=2
+ready=0
+until [[ $retry -eq 0 ]]  || [[ $ready -eq 1  ]]
+do
+	if aws --profile $AWS_PROFILE s3api wait object-exists --bucket ${BUCKET} --key ${KEY};
+	then
+		ready=1
+	else
+		let "retry--"
+	fi
+done
+[[ $ready -eq 1 ]] && exit 0 || echo "${BUCKET}/initial-cluster doesn't exist" && exit 1
