0c97d034ea71e3ae5db3bd76d65fd13aa074b26d
Xu Wang <xuwang@gmail.com>
2016-07-15 22:30:35 -0700

    Added rsync in plan_etcd to catch up all the changes from other terraform resources (worker, admiral) that depends on etcd.

--- resources/makefiles/etcd.mk
+++ resources/makefiles/etcd.mk
@@ -1,21 +1,33 @@
-this_make := $(lastword $(MAKEFILE_LIST))
-$(warning $(this_make))
 
 etcd: plan_etcd
+	@echo "#### Working on $@"
 	cd $(BUILD); $(TF_APPLY);
+	$(MAKE) upload_etcd_key
 	$(MAKE) get_etcd_ips
 
 plan_etcd: init_etcd
-	cd $(BUILD); $(TF_GET); $(TF_PLAN)
+	@echo "#### Working on $@"
+	cd $(BUILD); \
+	    for i in *.tf ; do \
+	      [[ -f $(RESOURCES)/terraforms/$$i ]] && rsync -avzq $(RESOURCES)/terraforms/$$i $(BUILD)/ ; \
+	    done; $(TF_GET); $(TF_PLAN)
 
-etcd_key:
+create_etcd_key:
+	@echo "#### Working on $@"
 	cd $(BUILD); \
 		$(SCRIPTS)/aws-keypair.sh -c $(CLUSTER_NAME)-etcd;
 
+upload_etcd_key:
+	@echo "#### Working on $@"
+	cd $(BUILD); \
+		$(SCRIPTS)/aws-keypair.sh -u $(CLUSTER_NAME)-etcd;
+
 destroy_etcd_key:
+	@echo "#### Working on $@"
 	cd $(BUILD); $(SCRIPTS)/aws-keypair.sh -d $(CLUSTER_NAME)-etcd;
 
 plan_destroy_etcd:
+	@echo "#### Working on $@"
 	$(eval TMP := $(shell mktemp -d -t etcd ))
 	mv $(BUILD)/etcd*.tf $(TMP)
 	cd $(BUILD); $(TF_PLAN)
@@ -23,14 +35,18 @@ plan_destroy_etcd:
 	rmdir $(TMP)
 
 destroy_etcd: destroy_etcd_key
+	@echo "#### Working on $@"
 	rm -f $(BUILD)/etcd*.tf
 	cd $(BUILD); $(TF_APPLY);
 
-init_etcd: init_vpc init_iam etcd_key
+init_etcd: init_vpc init_iam
+	@echo "#### Working on $@"
 	cp -rf $(RESOURCES)/terraforms/etcd*.tf $(BUILD)
+	$(MAKE) create_etcd_key 
 
 # Call this explicitly to re-load user_data
 update_etcd_user_data:
+	@echo "#### Working on $@"
 	cd $(BUILD); \
 		${TF_TAINT} aws_s3_bucket_object.etcd_cloud_config ; \
 		$(TF_APPLY)
@@ -39,4 +55,4 @@ get_etcd_ips:
 	@echo "etcd public ips: " `$(SCRIPTS)/get-ec2-public-id.sh etcd`
 
 .PHONY: etcd destroy_etcd plan_destroy_etcd plan_etcd init_etcd get_etcd_ips update_etcd_user_data
-.PHONY: destroy_etcd_key
+.PHONY: create_etcd_key upload_etcd_key destroy_etcd_key
