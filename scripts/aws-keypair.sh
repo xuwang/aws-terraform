a8f0a472e9d331c6a13953ad72a5b00c9b3ba646
Xu Wang <xuwang@gmail.com>
2016-07-15 22:27:10 -0700

    Separate create key and upload key functions. upload key should be called *after* terraform apply. This fixed bucket does not exist error when upload keys to s3 bucket.

--- scripts/aws-keypair.sh
+++ scripts/aws-keypair.sh
@@ -5,12 +5,18 @@ key='coreos-cluster'
 
 AWS_PROFILE=${AWS_PROFILE:-coreos-cluster}
 CLUSTER_NAME=${CLUSTER_NAME:-coreos-cluster}
-
+AWS_ACCOUNT=${AWS_ACCOUNT:-}
 AWS_REGION=${AWS_REGION:-us-west-2}
 
-echo "Getting AWS account number..."
-AWS_ACCOUNT=$(aws --profile ${AWS_PROFILE} iam get-user | jq ".User.Arn" | grep -Eo '[[:digit:]]{12}')
-echo $AWS_ACCOUNT
+# Default keypair name
+key="${CLUSTER_NAME}-default"
+
+
+if [ "X${AWS_ACCOUNT}" = "X" ];
+then
+  echo "Getting AWS account number..."
+  AWS_ACCOUNT=$(aws --profile ${AWS_PROFILE} iam get-user | jq ".User.Arn" | grep -Eo '[[:digit:]]{12}')
+fi
 
 TMP_DIR=keypairs
 
@@ -21,9 +27,8 @@ create(){
   else
     mkdir -p ${TMP_DIR}
     chmod 700 ${TMP_DIR}
-    echo "Creating keypair ${key} and uploading to s3"
+    echo "Creating keypair ${key}"
     aws --profile ${AWS_PROFILE} --region ${AWS_REGION} ec2 create-key-pair --key-name ${key} --query 'KeyMaterial' --output text > ${TMP_DIR}/${key}.pem
-    aws --profile ${AWS_PROFILE} --region ${AWS_REGION} s3 cp ${TMP_DIR}/${key}.pem s3://${AWS_ACCOUNT}-${CLUSTER_NAME}-config/keypairs/${key}.pem
     # copy the key to user's home .ssh
     # cp ${TMP_DIR}/${key}.pem ${HOME}/.ssh; chmod 600 ${HOME}/.ssh/${key}.pem
     chmod 600 ${TMP_DIR}/${key}.pem
@@ -32,6 +37,11 @@ create(){
     # Clean up
     # rm -rf ${TMP_DIR}
   fi
+}
+
+# Save a copy of the pem in s3 bucket
+upload(){
+    aws --profile ${AWS_PROFILE} --region ${AWS_REGION} s3 cp ${TMP_DIR}/${key}.pem s3://${AWS_ACCOUNT}-${CLUSTER_NAME}-config/keypairs/${key}.pem
 }
 
 destroy(){
@@ -54,7 +64,7 @@ destroy(){
   fi 
 }
 
-while getopts ":c:d:h" OPTION
+while getopts ":c:d:u:h" OPTION
 do
   key=$OPTARG
   case $OPTION in
@@ -63,6 +73,9 @@ do
       ;;
     d)
       destroy
+      ;;
+    u)
+      upload
       ;;
     *)
       echo "Usage: $(basename $0) -c|-d keyname"
