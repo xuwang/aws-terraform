c0c30031c695be9bc700b7b30c4cea1ef748a5dd
Xueshan Feng <xueshan-feng@stanford.edu>
2016-07-22 00:25:03 -0700

    tag vpc and subnet with cluster name.

--- resources/terraforms/vpc/vpc-subnet-admiral.tf
+++ resources/terraforms/vpc/vpc-subnet-admiral.tf
@@ -1,7 +1,7 @@
 module "admiral_subnet_a" {
   source = "../../modules/subnet"
 
-  subnet_name = "admiral_a"
+  subnet_name = "${var.cluster_name}-admiral_a"
   subnet_cidr = "10.10.2.0/26"
   subnet_az = "us-west-2a"
   vpc_id = "${aws_vpc.cluster_vpc.id}"
@@ -11,7 +11,7 @@ module "admiral_subnet_a" {
 module "admiral_subnet_b" {
   source = "../../modules/subnet"
 
-  subnet_name = "admiral_b"
+  subnet_name = "${var.cluster_name}-admiral_b"
   subnet_cidr = "10.10.2.64/26"
   subnet_az = "us-west-2b"
   vpc_id = "${aws_vpc.cluster_vpc.id}"
@@ -21,7 +21,7 @@ module "admiral_subnet_b" {
 module "admiral_subnet_c" {
   source = "../../modules/subnet"
 
-  subnet_name = "admiral_c"
+  subnet_name = "${var.cluster_name}-admiral_c"
   subnet_cidr = "10.10.2.128/26"
   subnet_az = "us-west-2c"
   vpc_id = "${aws_vpc.cluster_vpc.id}"
--- resources/terraforms/vpc/vpc-subnet-elb.tf
+++ resources/terraforms/vpc/vpc-subnet-elb.tf
@@ -1,7 +1,7 @@
 module "elb_subnet_a" {
   source = "../../modules/subnet"
 
-  subnet_name = "elb_a"
+  subnet_name = "${var.cluster_name}-elb_a"
   subnet_cidr = "10.10.3.0/26"
   subnet_az = "us-west-2a"
   vpc_id = "${aws_vpc.cluster_vpc.id}"
@@ -11,7 +11,7 @@ module "elb_subnet_a" {
 module "elb_subnet_b" {
   source = "../../modules/subnet"
 
-  subnet_name = "elb_b"
+  subnet_name = "${var.cluster_name}-elb_b"
   subnet_cidr = "10.10.3.64/26"
   subnet_az = "us-west-2b"
   vpc_id = "${aws_vpc.cluster_vpc.id}"
@@ -21,7 +21,7 @@ module "elb_subnet_b" {
 module "elb_subnet_c" {
   source = "../../modules/subnet"
 
-  subnet_name = "elb_c"
+  subnet_name = "${var.cluster_name}-elb_c"
   subnet_cidr = "10.10.3.128/26"
   subnet_az = "us-west-2c"
   vpc_id = "${aws_vpc.cluster_vpc.id}"
