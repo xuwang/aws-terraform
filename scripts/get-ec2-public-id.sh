fa1410d9c63305bb3f601db785d9cc5f3629cb2a
Xu Wang <xuwang@gmail.com>
2016-08-05 13:56:35 -0700

    redirect stderr to null suppress noisy.

--- scripts/get-ec2-public-id.sh
+++ scripts/get-ec2-public-id.sh
@@ -4,8 +4,8 @@ AWS_PROFILE=${AWS_PROFILE:-coreos-cluster}
 AWS_REGION=${AWS_REGION:-us-west-2}
 ASG=${1:-etcd}
 
-EC2_IDS=$(aws --profile $AWS_PROFILE --region $AWS_REGION autoscaling describe-auto-scaling-groups --auto-scaling-group-name $ASG \
-    | jq .AutoScalingGroups[0].Instances[].InstanceId | xargs)
+EC2_IDS=$(aws --profile $AWS_PROFILE --region $AWS_REGION autoscaling describe-auto-scaling-groups \
+	--auto-scaling-group-name $ASG 2> /dev/null | jq .AutoScalingGroups[0].Instances[].InstanceId | xargs)
 
 if [ ! -z "${EC2_IDS}" ]; then
 	aws --profile $AWS_PROFILE --region $AWS_REGION ec2 describe-instances --instance-ids $EC2_IDS \
