7b0627ef409f6710f617fc1fa4948ffbe006139b
Xu Wang <xuwang@gmail.com>
2016-08-20 13:04:12 -0700

    Simplify RDS databases DNS registration - no *.db subdomain is necessary for most cases.

--- resources/terraforms/rds/mysql.tf
+++ resources/terraforms/rds/mysql.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "cluster-mysql" {
     multi_az = "false" 
     availability_zone = "${var.rds_subnet_a_az}"
     port = "3306"
-    publicly_accessible = "false"
+    publicly_accessible = "true"
     backup_retention_period = "7"
     maintenance_window = "tue:10:33-tue:11:03"
     backup_window = "09:19-10:19"
@@ -21,49 +21,12 @@ resource "aws_db_instance" "cluster-mysql" {
     db_subnet_group_name = "${aws_db_subnet_group.cluster_db.id}"
 }
 
-# Register with Route53: 
-#    create a db.app_domain zone, a star *.db.app_domain record, NS record in app_domain zone
+# Register with Route53
 
-# In private zone *.mysqldb.cluster.local
-resource "aws_route53_record" "star_mysqldb_private" {
-    zone_id = "${var.route53_private_zone_id}"
-    name = "*.mysqldb"
-    type = "CNAME"
-    ttl = "60"
-    records = [ "${aws_db_instance.cluster-mysql.address}" ]
-}
-
-# mysql db NS delegation set
-resource "aws_route53_delegation_set" "mysql-dns" {
-    reference_name = "mysqlDNS"
-    provisioner "local-exec" {
-        command = "sleep ${var.wait_time}"
-    }
-}
-
-# Create mysql db zone record
-resource "aws_route53_zone" "cluster-mysqldb" {
-    name = "mysqldb.${var.app_domain}"
-    delegation_set_id = "${aws_route53_delegation_set.mysql-dns.id}"
-}
-# Create NS record in ${var.app_domain} zone
+# Create record in ${var.app_domain} zone
 resource "aws_route53_record" "mysqldb" {
     zone_id = "${var.route53_public_zone_id}"
     name = "mysqldb.${var.app_domain}"
-    type = "NS"
-    ttl = "60"
-    records = [
-        "${aws_route53_zone.cluster-mysqldb.name_servers.0}",
-        "${aws_route53_zone.cluster-mysqldb.name_servers.1}",
-        "${aws_route53_zone.cluster-mysqldb.name_servers.2}",
-        "${aws_route53_zone.cluster-mysqldb.name_servers.3}"
-    ]
-}
-
-# Create star_mysqldb record set
-resource "aws_route53_record" "star_mysqldb" {
-    zone_id = "${aws_route53_zone.cluster-mysqldb.id}"
-    name = "*.mysqldb.${var.app_domain}"
     type = "CNAME"
     ttl = "60"
     records = [ "${aws_db_instance.cluster-mysql.address}" ]
--- resources/terraforms/rds/postgres.tf
+++ resources/terraforms/rds/postgres.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "cluster-postgres" {
     multi_az = "false" 
     availability_zone = "${var.rds_subnet_a_az}"
     port = "5432"
-    publicly_accessible = "false"
+    publicly_accessible = "true"
     backup_retention_period = "7"
     maintenance_window = "tue:10:33-tue:11:03"
     backup_window = "09:19-10:19"
@@ -22,46 +22,11 @@ resource "aws_db_instance" "cluster-postgres" {
     db_subnet_group_name = "${aws_db_subnet_group.cluster_db.id}"
 }
 
-#Register with Route53
-resource "aws_route53_record" "star_postgresdb_private" {
-    zone_id = "${var.route53_private_zone_id}"
-    name = "*.postgresdb"
-    type = "CNAME"
-    ttl = "60"
-    records = [ "${aws_db_instance.cluster-postgres.address}" ]
-}
-
-# PostgresSQL
-resource "aws_route53_delegation_set" "postgres-dns" {
-    reference_name = "PostgresDNS"
-    provisioner "local-exec" {
-        command = "sleep ${var.wait_time}"
-    }
-}
-
-# Create postgresdb zone record
-resource "aws_route53_zone" "cluster-postgresdb" {
-    name = "postgresdb.${var.app_domain}"
-    delegation_set_id = "${aws_route53_delegation_set.postgres-dns.id}"
-}
-# Create NS record in ${var.app_domain} zone
+# Register with Route53
+# Create record in ${var.app_domain} zone
 resource "aws_route53_record" "postgresdb" {
     zone_id = "${var.route53_public_zone_id}"
     name = "postgresdb.${var.app_domain}"
-    type = "NS"
-    ttl = "60"
-    records = [
-        "${aws_route53_zone.cluster-postgresdb.name_servers.0}",
-        "${aws_route53_zone.cluster-postgresdb.name_servers.1}",
-        "${aws_route53_zone.cluster-postgresdb.name_servers.2}",
-        "${aws_route53_zone.cluster-postgresdb.name_servers.3}"
-    ]
-}
-
-# Create star_postgresdb record set
-resource "aws_route53_record" "star_postgresdb" {
-    zone_id = "${aws_route53_zone.cluster-postgresdb.id}"
-    name = "*.postgresdb.${var.app_domain}"
     type = "CNAME"
     ttl = "60"
     records = [ "${aws_db_instance.cluster-postgres.address}" ]
