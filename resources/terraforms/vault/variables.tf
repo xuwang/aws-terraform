086e5fba2ae38b7e1e8edd2bbafc13f4ae21ba5e
Xu Wang <xuwang@gmail.com>
2017-02-01 13:15:55 -0800

    update binary relase to 0.6.4.

--- resources/terraforms/vault/variables.tf
+++ resources/terraforms/vault/variables.tf
@@ -1,11 +1,12 @@
 variable "vault_release_url" {
-  default = "https://releases.hashicorp.com/vault/0.6.1/vault_0.6.1_linux_amd64.zip"
+  default = "https://releases.hashicorp.com/vault/0.6.4/vault_0.6.4_linux_amd64.zip"
 }
 variable "vault_cert" { default = "../certs/site.pem" }
 variable "vault_cert_chain" { default = "../certs/rootCA.pem" }
 variable "vault_cert_key" { default = "../certs/site-key.pem" }
+#  Use TCP because we use TLS. 
 variable "elb-health-check" {
-    default = "HTTP:8200/v1/sys/health"
+    default = "TCP"
     description = "Health check for Vault servers"
 }
 
